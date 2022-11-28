b1798727dde08a4d9a356127a8d2ecb084d315c1
Jaydeep Thakkar <84313869+jaydeep812@users.noreply.github.com>
2022-11-29 00:26:39 +0530

    Update Logo.jsx

--- src/assets/svg/Logo.jsx
+++ src/assets/svg/Logo.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import PractoSvg from "C:/Users/jayde/my-app/src/assets/img/practo-logo.svg";
+import PractoSvg from "../img/practo-logo.svg";
 
 function SvgComponent() {
   return (
